567f84948b8423a1eb1f63b36d278cb49b143a2c
Vadali Susmitha Sri Ramani <susmithasrvadali@gmail.com>
2025-06-26 19:49:00 +0530

    Create testbench.v

---
+++ Half Adder/testbench.v
@@ -0,0 +1,22 @@
+module half_adder_tb;
+  reg a,b;
+  wire sum,carry;
+  half_adder uut(
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+  );
+  initial begin 
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+  end
+  initial begin
+    a=1'b0;
+    b=1'b0;
+    #5 a=1'b0; b=1'b1;
+    #5 a=1'b1; b=1'b0;
+    #5 a=1'b1; b=1'b1;
+    $finish;
+  end
+endmodule
