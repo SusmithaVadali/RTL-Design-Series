92c9f2e86af18625941d49bf5df7dc9b93f6ae36
Vadali Susmitha Sri Ramani <susmithasrvadali@gmail.com>
2025-06-26 19:50:40 +0530

    Update testbench.v

--- Half Adder/testbench.v
+++ Half Adder/testbench.v
@@ -12,8 +12,7 @@ module half_adder_tb;
     $dumpvars(1);
   end
   initial begin
-    a=1'b0;
-    b=1'b0;
+    a=1'b0; b=1'b0;
     #5 a=1'b0; b=1'b1;
     #5 a=1'b1; b=1'b0;
     #5 a=1'b1; b=1'b1;
