8fd0102f7329bf68647ecb727af28abdfed1db25
Vadali Susmitha Sri Ramani <susmithasrvadali@gmail.com>
2025-06-30 21:39:45 +0530

    Delete Half Subtractor/half_subtractor.v

--- Half Subtractor/half_subtractor.v
+++
@@ -1,7 +0,0 @@
-module half_subtractor( input a,
-                        input b,
-                        output difference,
-                        output borrow);
-    assign difference = a^b;
-    assign borrow = (!a)&b;
-endmodule
