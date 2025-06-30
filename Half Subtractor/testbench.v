dff99690362b0493cfa9639dc65d2b7d29144548
Vadali Susmitha Sri Ramani <susmithasrvadali@gmail.com>
2025-06-30 21:40:36 +0530

    Update testbench.v

--- Half Subtractor/testbench.v
+++ Half Subtractor/testbench.v
@@ -11,9 +11,9 @@ module half_subtractor_tb;
       $dumpfile("half_subtractor.vcd");
       $dumpvars(0,half_subtractor_tb);
 
-      a = 1'b0; b = 1'b0; #10;
-      a = 1'b0; b = 1'b1; #10;
-      a = 1'b1; b = 1'b0; #10;
+      a = 1'b0; b = 1'b0; #10
+      a = 1'b0; b = 1'b1; #10
+      a = 1'b1; b = 1'b0; #10
       a = 1'b1; b = 1'b1; #10;
       $finish;
     end
